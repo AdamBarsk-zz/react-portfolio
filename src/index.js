1875ade692fc797743428f95e3ebc7ef501f27ab
Adam Ahlnander Barsk <adam.barsk@chas.se>
2018-03-15 15:32:37 +0100

    new min fa files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './style.css';
 import registerServiceWorker from './registerServiceWorker';
-import './static/fontawesome/fontawesome-all';
+import './static/fontawesome/fa-light.min';
+import './static/fontawesome/fontawesome.min';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
